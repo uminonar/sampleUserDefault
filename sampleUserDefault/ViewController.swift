659b19c26641a8f461f6dae92e3340e515225a62
Riho Okubo <rihoohkubo1002@gmail.com>
2016-05-02 18:29:21 +0800

    commit init

--- sampleUserDefault/ViewController.swift
+++ sampleUserDefault/ViewController.swift
@@ -10,11 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //ユーザーデフォルトから保存されたデータを取り出す
+        
+        var myDefault = NSUserDefaults.standardUserDefaults()
+        
+        //データを呼び出して
+        
+        var myStr = myDefault.stringForKey("myString")
+        
+        
+        //文字列が入っていたら、表示する
+        
+        print(myStr)
+        myTextField.text = myStr
     }
 
+   
+    @IBAction func tapReturn(sender: UITextField) {
+        //ユーザーデフォルトを用意する
+        var myDefault = NSUserDefaults.standardUserDefaults()
+        
+        //データを書き込んで
+        myDefault.setObject(sender.text, forKey: "myString")
+        
+        //即反映させる
+        myDefault.synchronize()
+        
+        
+        
+        
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
